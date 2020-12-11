0fb908e295c8bdd18ce63b19d46e4e5e8095368f
Yvonne Chen <yvonnechen1991@gmail.com>
2020-12-11 01:23:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,10 +8,10 @@ def introduction (name)
 end
 introduction ("Yvonne")
 
-def introduction_with_language (name, language = "Javascript")
+def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language ("Yvonne")
+introduction_with_language("Yvonne", "Javascript")
 
 def introduction_with_language_optional (name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
